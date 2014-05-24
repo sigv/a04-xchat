75b769465d90d05ee40a510d84499cb9b8fd2115
Valter Strods <valters.strods@gmail.com>
2014-05-24 22:07:46 +0300

    switched over to native calls for encoding and decoding

--- a04.py
+++ a04.py
@@ -2,14 +2,24 @@
 # Project: https://github.com/sigv/a04-xchat
 
 # Modify the following line to represent the location of the a04 project
-runnables = '/home/user/a04'
+projectpath = '/home/user/a04'
 
 __module_name__ = 'a04'
 __module_version__ = '0.1.0'
 __module_description__ = 'Provides simple a04 decoding and encoding support on the run'
 
 import xchat
-import subprocess
+
+import os
+os.sys.path.append(projectpath)
+
+encode = None
+decode = None
+try:
+	from encoder import encode
+	from decoder import decode
+except ImportError:
+	print 'Unable to import the main a04 project. Did you set the path?'
 
 def command_cb(word, word_eol, userdata):
 	msg = None
@@ -40,16 +50,14 @@ def command_cb(word, word_eol, userdata):
 		xchat.prnt('You must specify a key to ' + act + ': /a04 ' + act + ' --key <key>')
 		return xchat.EAT_ALL
 
-	try:
-		output = subprocess.check_output([ runnables + '/' + act + 'r.py', '--msg=' + msg, '--key=' + key, '--hash=' + hashsys ])
-	except OSError:
-		xchat.prnt('Unable to run the executable. Did you set the runnable path?')
-		return xchat.EAT_ALL
-	output = filter(None, output.split('\n'))
-	xchat.prnt(output[len(output) - 1]);
+	if act == 'encode':
+		xchat.prnt('encoded message: ' + ' '.join(encode(msg, key, hashsys)))
+	if act == 'decode':
+		xchat.prnt('decoded message: ' + decode(msg, key, hashsys))
 
 	return xchat.EAT_ALL
 
-xchat.hook_command('a04', command_cb, help='Usage: a04 encode|decode --msg <msg> --key <key> [--hash <hash>], prints the resulting message')
-print 'a04 has been loaded successfully'
+if encode is not None and decode is not None:
+	xchat.hook_command('a04', command_cb, help='Usage: a04 encode|decode --msg <msg> --key <key> [--hash <hash>], prints the resulting message')
+	print 'a04 has been loaded successfully'
 
